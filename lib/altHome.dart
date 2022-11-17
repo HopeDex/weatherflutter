a325a917e6dc3d67c0eb28066d51d5f509ced18a
Hope Dexter Madziakapita <48337899+HopeDex@users.noreply.github.com>
2022-11-17 06:32:41 +0200

    Create altHome.dart

---
+++ lib/altHome.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_glow/flutter_glow.dart';
+import './dataset.dart';
+import './detailPage.dart';
+import './extraWeather.dart';
+
+Weather currentTemp;
+Weather tomorrowTemp;
+List<Weather> todayWeather;
+List<Weather> sevenDay;
+String lat = "53.9006";
+String lon = "27.5590";
+String city = "Minisk";
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  getData() async {
+    fetchData(lat, lon, city).then((value) {
+      currentTemp = value[0];
+      todayWeather = value[1];
+      tomorrowTemp = value[2];
+      sevenDay = value[3];
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff030317),
+      body: currentTemp == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : SafeArea(
+              child: Column(
+                children: [
+                  CurrentWeather(getData),
+                  //  TodayWeather()
+                ],
+              ),
+            ),
+    );
+  }
+}
+
+class CurrentWeather extends StatefulWidget {
+  final Function() updateData;
+  CurrentWeather(this.updateData);
+  @override
+  _CurrentWeatherState createState() => _CurrentWeatherState();
+}
+
+class _CurrentWeatherState extends State<CurrentWeather> {
+  bool searchBar = false;
+  bool updating = false;
+  var focusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height - 30,
+      color: Color.fromRGBO(110, 241, 236, 1),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.location_pin,
+                  size: 40,
+                ),
+                Text(
+                  city,
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(children: [
+              Image(
+                image: AssetImage(currentTemp.image),
+                fit: BoxFit.fill,
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TodayWeather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 30, right: 30, top: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Today",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return DetailPage(tomorrowTemp, sevenDay);
+                  }));
+                },
+                child: Row(
+                  children: [
+                    Text(
+                      "7 days ",
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.grey,
+                      size: 15,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              bottom: 30,
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  WeatherWidget(todayWeather[0]),
+                  WeatherWidget(todayWeather[1]),
+                  WeatherWidget(todayWeather[2]),
+                  WeatherWidget(todayWeather[3])
+                ]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class WeatherWidget extends StatelessWidget {
+  final Weather weather;
+  WeatherWidget(this.weather);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          border: Border.all(width: 0.2, color: Colors.white),
+          borderRadius: BorderRadius.circular(35)),
+      child: Column(
+        children: [
+          Text(
+            weather.current.toString() + "\u00B0",
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Image(
+            image: AssetImage(weather.image),
+            width: 50,
+            height: 50,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            weather.time,
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}
