a073b54d6bfff95ad00a3f539dfdfdb644a5f708
Hope Dexter Madziakapita <48337899+HopeDex@users.noreply.github.com>
2022-11-17 08:41:39 +0200

    altHome

--- lib/altHome.dart
+++ lib/altHome.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_glow/flutter_glow.dart';
@@ -12,6 +14,7 @@ List<Weather> sevenDay;
 String lat = "53.9006";
 String lon = "27.5590";
 String city = "Minisk";
+var colorb = Colors.blue[100];
 
 class HomePage extends StatefulWidget {
   @override
@@ -47,7 +50,7 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 children: [
                   CurrentWeather(getData),
-                  //  TodayWeather()
+                  // TodayWeather(),
                 ],
               ),
             ),
@@ -69,94 +72,162 @@ class _CurrentWeatherState extends State<CurrentWeather> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height - 30,
-      color: Color.fromRGBO(110, 241, 236, 1),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.location_pin,
-                  size: 40,
-                ),
-                Text(
-                  city,
-                  style: TextStyle(color: Colors.black, fontSize: 24),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Column(children: [
-              Image(
-                image: AssetImage(currentTemp.image),
-                fit: BoxFit.fill,
-              ),
-            ]),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TodayWeather extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 30, right: 30, top: 10),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Today",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (BuildContext context) {
-                    return DetailPage(tomorrowTemp, sevenDay);
-                  }));
-                },
-                child: Row(
-                  children: [
-                    Text(
-                      "7 days ",
-                      style: TextStyle(fontSize: 18, color: Colors.grey),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios_outlined,
-                      color: Colors.grey,
-                      size: 15,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              bottom: 30,
-            ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    if (currentTemp.current > 4) {
+      colorb = Colors.red;
+    } else {}
+    return Expanded(
+      child: Container(
+        color: colorb,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
                 children: [
-                  WeatherWidget(todayWeather[0]),
-                  WeatherWidget(todayWeather[1]),
-                  WeatherWidget(todayWeather[2]),
-                  WeatherWidget(todayWeather[3])
-                ]),
-          )
-        ],
+                  Icon(
+                    Icons.location_pin,
+                    size: 40,
+                  ),
+                  Text(
+                    city,
+                    style: TextStyle(color: Colors.black, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Stack(children: [
+                Image(
+                  image: AssetImage(currentTemp.image),
+                  fit: BoxFit.fill,
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          currentTemp.name,
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        Text(
+                          currentTemp.current.toString() + '\u00B0',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 100,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.35),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.wind_power_outlined,
+                                    size: 30,
+                                  ),
+                                  Text(
+                                    currentTemp.wind.toString() + " Km/h",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.water_drop_outlined,
+                                    size: 30,
+                                  ),
+                                  Text(
+                                    currentTemp.wind.toString() + " %",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Today",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return DetailPage(tomorrowTemp, sevenDay);
+                          }));
+                        },
+                        child: Row(
+                          children: [
+                            Text(
+                              "7 days ",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios_outlined,
+                              color: Colors.grey,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      bottom: 30,
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          WeatherWidget(todayWeather[0]),
+                          WeatherWidget(todayWeather[1]),
+                          WeatherWidget(todayWeather[2]),
+                        ]),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -168,15 +239,17 @@ class WeatherWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 150,
       padding: EdgeInsets.all(15),
       decoration: BoxDecoration(
+          color: Color.fromARGB(107, 255, 255, 255),
           border: Border.all(width: 0.2, color: Colors.white),
           borderRadius: BorderRadius.circular(35)),
       child: Column(
         children: [
           Text(
-            weather.current.toString() + "\u00B0",
-            style: TextStyle(fontSize: 20),
+            weather.time,
+            style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
           ),
           SizedBox(
             height: 5,
@@ -186,13 +259,17 @@ class WeatherWidget extends StatelessWidget {
             width: 50,
             height: 50,
           ),
+          Text(
+            weather.current.toString() + "\u00B0",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           SizedBox(
             height: 5,
           ),
-          Text(
-            weather.time,
-            style: TextStyle(fontSize: 16, color: Colors.grey),
-          )
         ],
       ),
     );
